5ca9e298e862839f13bec8de23a2c0964db9bddf
Brian Redd <bredd@trinidadbenham.com>
2023-10-28 18:27:20 -0600

    basic ui/api scaffolding

---
+++ ui/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import MainContainer from "./components/MainContainer";
+
+import "./styles/App.scss";
+
+const App = () => (
+  <div data-test="container-app">
+    <MainContainer />
+  </div>
+);
+
+export default App;
